90a6d8ed63ed0772066357ce70dcbae4a0aad6cb
Nazarelis Ojeda <106442997+Nazarelis@users.noreply.github.com>
2023-06-24 19:03:23 -0300

    Add files via upload

--- heladeria.py
+++ heladeria.py
@@ -4,10 +4,13 @@ import random
 
 def validarNombreHelado (lista):
     helado = input("Ingrese nombre del helado, solo debe contener letras, FIN para finalizar: ")
-    helado=helado.upper()
+    while not helado:
+        helado = input("Ingrese nombre del helado, solo debe contener letras, FIN para finalizar: ")
+
+    helado = helado.upper()
     while helado.isalpha() == False or helado in lista:
         helado = input("Error ingrese nombre del helado, solo debe contener letras, FIN para finalizar: ")
-        helado=helado.upper()
+        helado = helado.upper()
     return helado
 
 def validarNumero(mensaje1, mensaje2):
@@ -25,6 +28,16 @@ def validarNumero(mensaje1, mensaje2):
         if (numeroValido != None and numeroValido > 0) or error == False:
             break
     return numeroValido
+
+
+
+def validarCodigoHelado(listaCod):
+    codigo = input("Ingrese codigo de helado. FIN para finalizar.  ") ###VALIDAR: codigo de helado tiene que ser < al len de la lista y mayor a 0
+    while codigo.upper() != "FIN" and codigo > len(listaCod):
+        codigo = input("Ingrese codigo de helado. FIN para finalizar.  ")
+
+    return codigo
+
 
 def llenarMatriz(filas, columnas, m):    
     for i in range(filas):
@@ -64,37 +77,46 @@ def mostrarListaHelados(listaHelados):
 # FIN FUNCIONES
 
 
-# PROGRAMA PRINCIPAL
+### PROGRAMA PRINCIPAL ###
 
-listaHelados = []
+# CARGA ARCHIVO DE ENTRADA
+tuplaHelados = ()
+listaCodigos = []
 id = 0
 try:
     archHelados = open("helados.csv", "wt")
 except IOError:
     print("No se pudo crear el archivo de Helados")
 else:
-    helado = validarNombreHelado(listaHelados)
-    contadorHelado=1
+    helado = validarNombreHelado(tuplaHelados)
+    
     while helado !="FIN":
-        listaHelados.append(helado)
+        tuplaHelados = tuplaHelados + (helado,)
         descripcion = input("Ingrese una descripcion del helado: ")
+        while not descripcion:
+            descripcion = input("Ingrese una descripcion del helado: ")
         stockInicial = validarNumero("Ingresar numero de stock mayor a cero: ", "Error. Ingresar numero de stock mayor a cero: ") 
+
+        while not stockInicial:
+            stockInicial = validarNumero("Ingresar numero de stock mayor a cero: ", "Error. Ingresar numero de stock mayor a cero: ") 
         archHelados.write(str(id) + ";" + helado + ";" + descripcion + ";" + str(stockInicial) + "\n")
-        helado = validarNombreHelado(listaHelados)
+        listaCodigos.append(id)
+        helado = validarNombreHelado(tuplaHelados)
         id +=1
+        
     archHelados.close()
+# FIN CARGA DE ARCHIVO DE ENTRADA
 
-
-print(listaHelados)
+#print(listaHelados)
 
 m = []
-lineas = len(listaHelados)
+lineas = len(tuplaHelados)
 
 llenarMatriz(lineas, 4, m)
 pasarMatriz(m)
 listaVentas = [0] * lineas
 
-mostrarListaHelados(listaHelados)
+mostrarListaHelados(tuplaHelados)
 
 precioHelado = 700                                     ###Precio para la porcion de helado (valor arbitrario)
 recaudacionTotal = 0                                   ###Inicializacion de la recaudacion en 0
@@ -106,35 +128,44 @@ stockLimite = 10
 ### INICIO CARGA VENTAS
 
 ingresoHelado = input("Ingrese codigo de helado. FIN para finalizar.  ") ###VALIDAR: codigo de helado tiene que ser < al len de la lista y mayor a 0
+while not ingresoHelado:
+    ingresoHelado = input("Ingrese codigo de helado. FIN para finalizar.  ")
 
-while ingresoHelado != "FIN":
-    ##Recorrer aqui la matriz para saber si hay suficiente en stock
-    stock = m[int(ingresoHelado)][3]
-    cantidadVenta = validarNumero("Ingresar cantidad vendida ", "Error. Cantidad de venta debe ser mayor a cero, reingresar: ") ### numero entero mayor a 0
-    while (cantidadVenta * vaso) > stock:
-        cantidadVenta = int(input("Cantidad vendida es mayor al stock disponible, reingrese cantidad vendida: ")) ### numero entero mayor a 0
 
-    
+while ingresoHelado != "FIN" and ingresoHelado!="fin":
+    try:
+        stock = m[int(ingresoHelado)][3]
+    except ValueError:
+        break
+    else:
+        cantidadVenta = validarNumero("Ingresar cantidad vendida ", "Error. Cantidad de venta debe ser mayor a cero, reingresar: ") ### numero entero mayor a 0
+        while (cantidadVenta * vaso) > stock:
+            cantidadVenta = validarNumero("Cantidad vendida es mayor al stock disponible, reingrese cantidad vendida: ", "Error. Cantidad de venta debe ser mayor a cero, reingresar: ") ### numero entero mayor a 0
+            
     listaVentas[int(ingresoHelado)] += cantidadVenta
-    m[int(ingresoHelado)][3] -= cantidadVenta*vaso
+    m[int(ingresoHelado)][3] -= cantidadVenta * vaso
     contadorVentas += cantidadVenta
     recaudacionTotal += (precioHelado * cantidadVenta)                      ### acumulacion de la recaudacion total
 
     ingresoHelado = input("Ingrese codigo de helado. FIN para finalizar.  ") ###VALIDAR: codigo de helado tiene que ser < al len de la lista y mayor a 0
-
 # FIN CARGA VENTAS
 
+
 # MUESTRA EN PANTALLA DE RESULTADOS AL FINAL DEL DIA
-print("La cantidad de ventas al final del día fue ", contadorVentas)
-print("La recaudación total al final del día fue ", recaudacionTotal, "$")
 
-# Muestra de helados con mayor y menor venta
-menorVenta = listaVentas.index(min(listaVentas))                     ### indice de menor venta
-mayorVenta = listaVentas.index(max(listaVentas))                     ### indice de mayor venta
+if contadorVentas > 0:
+    menorVenta = listaVentas.index(min(listaVentas))                     ### indice de menor venta
+    mayorVenta = listaVentas.index(max(listaVentas))                     ### indice de mayor venta
 
-print("El helado con mayor venta fue", listaHelados[mayorVenta])
-print("El helado con menor venta fue", listaHelados[menorVenta])
+    # Muestra de helados con mayor y menor venta
+    print("El helado con mayor venta fue", tuplaHelados[mayorVenta])
+    print("El helado con menor venta fue", tuplaHelados[menorVenta])
 
+    # Muestra cantidad total de ventas y recaudación
+    print("La cantidad de ventas al final del día fue ", contadorVentas)
+    print("La recaudación total al final del día fue ", recaudacionTotal, "$")
+else:
+    print("No se han registrado ventas")
 
 stockTotal = 0                                                      ### calculamos la suma del stock total de los helados
 for i in range(lineas):
@@ -143,8 +174,8 @@ for i in range(lineas):
         print("Helado sabor", m[i][2], "necesita reposicion de stock\n")
 
 
-print(m)
 
+# CARGA ARCHIVO DE SALIDA
 try:
     archFinal = open("Final.csv", "wt")
 except IOError:
@@ -153,3 +184,4 @@ else:
     for i in range(lineas):
         archFinal.write(str(m[i][1]) + ";" + str(m[i][3])+ ";" + str(stockLimite) + ";" + str(int((m[i][3]/stockTotal)*100))+"%" + "\n")
     archFinal.close()
+# FIN CARGA ARCHIVO SALIDA
